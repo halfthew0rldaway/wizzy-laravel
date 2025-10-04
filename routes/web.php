75ff2de5805266fc66bf0c03c00b5fbed567a1ad
halfthew0rldaway <wisnuwidyapradana2004@gmail.com>
2025-10-04 10:10:29 +0700

    p4

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\logincontroller;
-use App\http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [LoginController::class, 'index'])->name('login');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login_process');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
